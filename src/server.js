49bd7ea2cdced93f745fb5a72da3777d3d6caf5d
Adao-Angelo <adaobegginer@gmail.com>
2024-02-05 01:42:48 +0100

    config  nodemailer sent to

--- src/server.js
+++ src/server.js
@@ -3,6 +3,22 @@ const nodemailer = require("nodemailer");
 const app = express();
 
 app.get("/", (req, res) => {
+  const smtp = nodemailer.createTestAccount({
+    host: "smtp.gamil.com",
+    port: 587,
+    secure: true,
+    auth: {
+      user: "adaobegginer@gmil.com",
+      pass: process.env.PASS,
+    },
+  });
+
+  const configEmail = {
+    form: "adaobegginer@gmil.com",
+    to: "adaobegginer@gmil.com",
+    subject: "emplememting nodemail",
+    html: "<a src='www.facebool.com'> Opem Facebok</a>",
+  };
   res.send("message: server running");
 });
 
