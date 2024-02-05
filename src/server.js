78db7779caae5cf248828749dfbf3121bcc7e29d
Adao-Angelo <adaobegginer@gmail.com>
2024-02-05 01:20:33 +0100

    config  nodemailer

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("message: server running");
+});
 
 app.listen(3000, () => {
   console.log("server is running at port 3000");
